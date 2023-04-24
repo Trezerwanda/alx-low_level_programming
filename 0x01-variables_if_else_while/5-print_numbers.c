7c90a7d0f8e03a179885a1c337e4ad9540b186bb
“Trezerwanda” <konyangotrezer@students.uonbi.ac.ke>
2023-04-24 11:49:36 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - print all single digit numbers of base 10
+ * Return: always 0
+ */
+int main(0)
+{
+int num;
+for (num = 0; num < 10; num++)
+printf("%d", num);
+printf("\n");
+return (0);
+}
