e51be2c4bebc233266ced7a2a469d606ef615d01
“Trezerwanda” <konyangotrezer@students.uonbi.ac.ke>
2023-05-04 13:34:49 -0700

    task1edited

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _strncat - concentrate 2 strings using at most x bytes from src
+ * _strncat - concentrate 2 strings using at most n bytes from src
  * @dest: input value
  * @src: input value
  * @n: input value
@@ -12,12 +12,12 @@ char *_strncat(char *dest, char *src, int n)
 int i;
 int j;
 i = 0;
-while (dest[i] != '/0')
+while (dest[i] !='\0')
 {
 i++;
 }
 j = 0;
-while (j < x && src[j] != '/0')
+while (j < n && src[j] !='\0')
 {
 dest[i] = src[j];
 i++;
