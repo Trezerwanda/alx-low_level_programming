61e9f16e402ce0d61f4738cf5532fd977ffe4629
“Trezerwanda” <konyangotrezer@students.uonbi.ac.ke>
2023-05-10 13:46:09 -0700

    edits

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -17,7 +17,7 @@ return (check_pal(s, 0, _strlen_recursion(s)));
  */
 int _strlen_recursion(char *s)
 {
-if (s == '\0')
+if (*s == '\0')
 return (0);
 return (1 + _strlen_recursion(s + 1));
 }
@@ -28,11 +28,11 @@ return (1 + _strlen_recursion(s + 1));
  * @l: length of string
  * Return: 1 if palindrome, 0 if not
  */
-int check_pal(char *s, int i, int len)
+int check_pal(char *s, int i, int l)
 {
-if (*(s + i) != *(s + len - 1))
+if (*(s + i) != *(s + l - 1))
 return (0);
-if (i >= len)
+if (i >= l)
 return (1);
-return (check_pal(s, i + 1, len - 1));
+return (check_pal(s, i + 1, l - 1));
 }
