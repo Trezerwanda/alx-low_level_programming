e07b692f9ec59868b09500ccde73ebbf3d5a9692
“Trezerwanda” <konyangotrezer@students.uonbi.ac.ke>
2023-05-10 10:27:04 -0700

    task0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,15 +2,15 @@
 /**
  * _puts_recursion - prints a string folowed by a new line
  * @s: the string to be printed
- * Return: void
+ * Return: no return
  */
 void _puts_recursion(char *s)
 {
-if (s == '\0')
+if (*s != 0)
 {
-_putchar('\n');
-return;
-}
 _putchar(*s);
 _puts_recursion(s + 1);
+}
+else
+_putchar('\n');
 }
