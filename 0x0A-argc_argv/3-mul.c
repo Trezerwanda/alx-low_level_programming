09ac227287695df7a1f0bac9b6b3474501408cdd
“Trezerwanda” <konyangotrezer@students.uonbi.ac.ke>
2023-05-12 12:59:22 -0700

    3edits

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,15 +8,15 @@
 * @argv: argument vector
 * Return: 0 if true, 1 if false
 */
-int main(int argc, char argv[])
-{
+int main(int argc, char *argv[])
+{	
 if (argc != 3)
 {
 printf("Error\n");
 return (1);
 }
-int a = atoi(&argv[1]);
-int b = atoi(&argv[2]);
+int a = atoi(&argv[1][0]);
+int b = atoi(&argv[2][0]);
 int result = a * b;
 printf("%d", result);
 return (0);
